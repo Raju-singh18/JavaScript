0d4e9ac4e07188e2f994c88393a0c0a6f0c10b40
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-05 00:50:30 +0530

    Data Type Conversion Added

---
+++ DataTypeConversion/index.js
@@ -0,0 +1,84 @@
+
+//! 1. Number
+// let num = "123"
+// console.log(typeof num);
+// let ValueToNumber = Number(num); //Convert string to number
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+// let num = "123xyz"
+// console.log(typeof num);
+// let ValueToNumber = Number(num);
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+// let num = null;
+// console.log(typeof num);
+// let ValueToNumber = Number(num);
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+// let num = undefined;
+// console.log(typeof num);
+// let ValueToNumber = Number(num);
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+// let num = true
+// console.log(typeof num);
+// let ValueToNumber = Number(num);
+// console.log(ValueToNumber);
+// console.log(typeof ValueToNumber);
+
+//! 2. Boolean
+
+// let bool = true;
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = 1;
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = 1232;
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = 0;
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = "0";
+// console.log(typeof bool);
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = null;
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+// let bool = undefined;
+// console.log(typeof bool)
+// let ValueToBoolean = Boolean(bool);
+// console.log(ValueToBoolean);
+// console.log(typeof ValueToBoolean);
+
+//! 3. String
+
+// let str = 123
+// console.log(typeof str);
+// let ValueToString = String(str);
+// console.log(ValueToString);
+// console.log(typeof ValueToString);
+
+let str = true;
+console.log(typeof str);
+let ValueToString = String(str);
+console.log(ValueToString);
+console.log(typeof ValueToString);
