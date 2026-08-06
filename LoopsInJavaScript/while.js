5bd42b667819025ed619001ed31967e2b47e4118
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-06 16:17:30 +0530

    Modified while.js file

--- LoopsInJavaScript/while.js
+++ LoopsInJavaScript/while.js
@@ -13,8 +13,17 @@
 // j++;
 // }while(j<5);
 
-let z=5;
+// let z=5;
+// do{
+// console.log(z);
+// z++;
+// }while(z<5);
+
+
+//! Reasl life example
+let password="";
+let correctPassword="admin"
 do{
-console.log(z);
-z++;
-}while(z<5);
+ password=prompt("Enter your password");
+}while(password !== correctPassword);
+console.log("Access granted");
