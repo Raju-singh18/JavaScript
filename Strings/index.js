14c78f49cabdac66590d3540df8a63e821ff4f20
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-06 18:13:56 +0530

    Strings Added

---
+++ Strings/index.js
@@ -0,0 +1,49 @@
+
+// let name = "Raju Singh $ 123"
+// console.log(name);
+
+// ! Declaration of string
+// let name1 = "Rakesh Kumar"
+// let name2 = 'Rakesh kumar'
+// let name3 = `Rakesh kumar 2` //ES6
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+
+//! Cancatination of string
+// let firstName="Mohit"
+// let lastName="Singh"
+// console.log(firstName + " " + lastName);
+
+// template literal
+// let fullName = `${firstName} Kumar ${lastName}`
+// console.log(fullName);
+
+//! Escape Charater
+// New Line \n
+let text = "Hello\nWorld";
+console.log(text);
+
+//Backslash single quate \'
+let text1 = "Hello\'World";
+console.log(text1);
+let text2 = "it\'a good day"
+console.log(text2);
+
+
+// Double Quates
+let text3 = "It\"a good day"
+console.log(text3);
+let text4 = "He said \"Hello\""
+console.log(text4);
+
+// backslash \\
+let text5 = "This is a backslash \\"
+console.log(text5);
+
+
+// Dynamic string using template literals
+let name="Rohit"
+let age=22
+let text6 = `My name is ${name} and my age is ${age}`;
+console.log(text6)
---
+++ Strings/methods.js
@@ -0,0 +1,54 @@
+
+// ! length
+// let str = "Raju Singh"
+// console.log(str.length);
+
+//! Sting to lowercase and uppercase
+// let str1 = "Raju Singh"
+// console.log(str1.toUpperCase());
+// console.log(str1.toLowerCase());
+
+//! charAt
+// let str2 = "Raju"
+// console.log(str2.charAt(3));
+
+// ! indexOf
+// let str3 = "Rohit"
+// console.log(str3.indexOf("h"));
+
+// ! includes
+// let str4 = "Mohit"
+// console.log(str4.includes('h'))
+// console.log(str4.includes('la'))
+
+// ! slice vs subString
+let str5 = "Mohit kumar"
+// console.log(str5.slice(0,5));
+// console.log(str5.substring(0,5));
+
+// console.log(str5.slice(5,0));
+// console.log(str5.substring(5,0));
+
+// console.log(str5.slice(-5));
+// console.log(str5.substring(-5));
+
+// ! replace
+// let str6 = "Mohit Kumar"
+// console.log(str6.replace("Kumar", "Singh"));
+
+// ! split
+// let str7 = "Mohit kumar"
+// console.log(str7.split(" "));
+
+// let str8 = "HTML,CSS,JS"
+// console.log(str8.split(","));
+
+// ! trim()
+// let str9="   Monit  "
+// console.log(str9.trim());
+
+// Concate string
+let str10 = "Mohit";
+let str11 =" kumar";
+let str12 = str10.concat(str11);
+console.log(str12);
