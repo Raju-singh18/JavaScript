3376515e361bd422482c1c8834a736a3b7593b6d
Raju Singh <rajusingh.mca26@gmail.com>
2026-08-09 11:44:25 +0530

    Modules Added

---
+++ Modules/file1.js
@@ -0,0 +1,12 @@
+//!  import fromm file2.js
+
+// import { name,add } from "./file2.js";
+// console.log(name);
+// console.log(add(4,6));
+
+
+//! importing default function
+// import add from "./file2.js";
+// console.log(add(5,3));
+import addxyz from "./file2.js";
+console.log(addxyz(4,8));
---
+++ Modules/file2.js
@@ -0,0 +1,10 @@
+// ! Export from Modules (Named Export)
+// export const name = "Raju Singh"
+// export function add(a,b){
+//     return a+b;
+// }
+
+//! exporting defauult 
+export default function add(a,b){
+    return a+b;
+}
